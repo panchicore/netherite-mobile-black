b4a4207d76b9d4501e0f5b1e80b3f94eb7fa327f
panchicore <thepanchi@gmail.com>
2022-11-12 16:47:03 -0500

    working tabs

--- src/Navigators/Main.tsx
+++ src/Navigators/Main.tsx
@@ -1,32 +1,38 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { ExampleContainer, EvaContainer } from '@/Containers'
+import {
+  BottomNavigation,
+  BottomNavigationTab,
+  Icon,
+} from '@ui-kitten/components'
+import { EvaContainer } from '@/Containers'
+import { OperationsNavigator } from '@/Navigators/Operations'
 
-const Tab = createBottomTabNavigator()
+const { Navigator, Screen } = createBottomTabNavigator()
 
-// @refresh reset
-const MainNavigator = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="Home"
-        component={ExampleContainer}
-        options={{
-          tabBarIconStyle: { display: 'none' },
-          tabBarLabelPosition: 'beside-icon',
-        }}
-      />
-      <Tab.Screen
-        name="Home2"
-        component={EvaContainer}
-        options={{
-          tabBarIconStyle: { display: 'none' },
-          tabBarLabelPosition: 'beside-icon',
-        }}
-      />
+const TruckIcon = props => <Icon {...props} name="car-outline" />
+const ArchiveIcon = props => <Icon {...props} name="archive-outline" />
+const BarIcon = props => <Icon {...props} name="bar-chart-outline" />
 
-    </Tab.Navigator>
-  )
-}
+const BottomTabBar = ({ navigation, state }) => (
+  <BottomNavigation
+    selectedIndex={state.index}
+    onSelect={index => navigation.navigate(state.routeNames[index])}
+  >
+    <BottomNavigationTab title="Operaciones" icon={TruckIcon} />
+    <BottomNavigationTab title="Bases" icon={ArchiveIcon} />
+    <BottomNavigationTab title="Informes" icon={BarIcon} />
+  </BottomNavigation>
+)
+
+const TabNavigator = () => (
+  <Navigator tabBar={props => <BottomTabBar {...props} />}>
+    <Screen name="Operaciones" component={OperationsNavigator} />
+    <Screen name="Bases" component={EvaContainer} />
+    <Screen name="Informes" component={EvaContainer} />
+  </Navigator>
+)
+
+const MainNavigator = () => <TabNavigator />
 
 export default MainNavigator
